2b4d3db5b9f633511b42ba87746625a76f735dcf
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-11-19 23:41:13 +0700

    add: hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import './style/style.css'
 import React from 'react';
-import Navbar from './components/navbar/Navbar';
+import Navbar from './components/Navbar';
+import HeroSection from './components/HeroSection';
 
 function App() {
   return (<>
-    <div className="body bg-[#161616]">
+    <div className="body bg-[#161616] mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       <Navbar />
+      <HeroSection />
     </div>
   </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,30 @@
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import logo from '../../assets/image/logo.png'
+import logo from '../assets/image/logo.png'
 
-const navigation = [
-  { name: 'Home', href: '#', current: false },
-  { name: 'About', href: '#', current: false },
-  { name: 'Project', href: '#', current: false },
-  { name: 'Contact', href: '#', current: false },
-]
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
 
 const Navbar = () => {
+  const navigation = [
+    { name: 'Home', href: '#', current: false },
+    { name: 'About', href: '#', current: false },
+    { name: 'Project', href: '#', current: false },
+    { name: 'Contact', href: '#', current: false },
+  ]
+  
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+  }
+
+
   return (<>
     <Disclosure as="nav" className="bg-[#161616]">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-sm p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
@@ -31,7 +33,7 @@ const Navbar = () => {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start md:justify-between ">
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-between ">
                 <div className="flex flex-shrink-0 items-center">
                   <a href="#">
                     <img
@@ -49,7 +51,7 @@ const Navbar = () => {
                         href={item.href}
                         className={classNames(
                           item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'px-3 py-2 rounded-md text-sm font-medium hover:bg-[#6B3BCB]'
+                          'px-3 py-2 rounded-sm text-sm font-medium hover:bg-[#6B3BCB]'
                         )}
                         aria-current={item.current ? 'page' : undefined}
                       >
