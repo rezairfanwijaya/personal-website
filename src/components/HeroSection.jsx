31ca0f495a1d1e1aadd42514684a95f73f69f58c
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-11-20 08:50:30 +0700

    update: animation and grid

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,14 +2,15 @@ import herosectionImage from '../assets/image/herroSection.png'
 import emailImage from '../assets/image/email.svg'
 import linkedinImage from '../assets/image/linkedin.svg'
 import githubImage from '../assets/image/github.svg'
+import astronot from '../assets/animation/astronot.gif'
 
 const HeroSection = () => {
     return (
         <>
-            <div className="hero-section my-5 grid grid-cols-1 gap-11 md:grid-cols-2 md:my-16 lg:gap-0">
+            <div className="hero-section my-5 grid grid-cols-1 gap-11 md:grid-cols-2 md:my-0 lg:gap-0 place-items-center">
                 {/* hero image */}
                 <div className="left">
-                    <img src={herosectionImage} alt="hero-image" className='md:h-96 lg:h-max lg:ml-8' />
+                    <img src={astronot} alt="hero-image" className='h-max md:h-96 lg:h-max lg:ml-8' />
                 </div>
 
                 {/* intro */}
@@ -18,7 +19,7 @@ const HeroSection = () => {
                         <p className='text-[#8D63E1] font-bold text-md lg:text-xl'>Hello, Im Reza Irfan Wijaya</p>
                     </div>
                     <div className="role">
-                        <p className='text-white font-black text-4xl lg:text-7xl'>Web Developer</p>
+                        <p className='text-white font-black text-5xl lg:text-7xl'>Web Developer</p>
                     </div>
                     <div className="intro text-[#C0C0C0] text-md my-5 leading-7 lg:my-6">
                         <p>Hello, I am an informatic engineering student who has an interest in website development. For now I'm focusing on learning fullstack with Reacjs technology as the frontend and Golang as the backend.</p>
